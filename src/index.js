753edd49cdd1d0c6b4a28d9a08499337537c70b1
mixlos <michalop@gmail.com>
2017-03-07 23:49:49 +0200

    create StorePicker Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+render(
+  <StorePicker />,
+  document.getElementById('main')
+);
