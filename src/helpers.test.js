5e513c3f9ad0d525c0c995197e5a51fffc075dc6
mixlos <michalop@gmail.com>
2017-03-08 19:30:09 +0200

    changed to euro sign and in helpers and helpers.test added getFunName() for storeIds in StorePicker

--- src/helpers.test.js
+++ src/helpers.test.js
@@ -1,20 +1,20 @@
 import { formatPrice, slugify } from './helpers';
 
-it('should convert cents to dollars', () => {
+it('should convert cents to euros', () => {
   const actual = formatPrice(230);
-  const expected = '$2.30';
+  const expected = '€2.30';
   expect(actual).toBe(expected);
 });
 
 it('should convert negative cents values correctly', () => {
   const actual = formatPrice(-2);
-  const expected = '$-0.02';
+  const expected = '€-0.02';
   expect(actual).toBe(expected);
 });
 
 it('should convert zero values correctly', () => {
   const actual = formatPrice(0);
-  const expected = '$0.00';
+  const expected = '€0.00';
   expect(actual).toBe(expected);
 
   const actualNeg = formatPrice(-0);
