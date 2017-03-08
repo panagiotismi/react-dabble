c627306d23f8fb9c7f7d005f6d8c81dc3dcd50a4
mixlos <michalop@gmail.com>
2017-03-08 14:40:59 +0200

    added conditional routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
+import Root from './components/Root';
 import './css/style.css';
 
 render(
-  <App />,
-  document.getElementById('main')
+  <Root />, document.getElementById('main')
 );
