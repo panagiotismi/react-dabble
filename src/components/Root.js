48b0777a4dd8a50dc25d3aa3a798954d4296fc2f
mixlos <michalop@gmail.com>
2017-03-16 17:28:43 +0200

    hack for deploying to github pages

--- src/components/Root.js
+++ src/components/Root.js
@@ -4,8 +4,11 @@ import StorePicker from './StorePicker';
 import App from './App';
 import NotFound from './NotFound';
 
+// little hack for deploy in github pages
+const repo = `/${window.location.pathname.split('/')[1]}`;
+
 const Root = () => (
-  <BrowserRouter>
+  <BrowserRouter basename={repo}>
     <Switch>
       <Route exact path="/" component={StorePicker} />
       <Route path="/store/:storeId" component={App} />
